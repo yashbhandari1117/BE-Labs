a80fcb0e7c1caa63767b79ef27fe640971ad8d71
yashbhandari1117 <45139703+yashbhandari1117@users.noreply.github.com>
2022-03-08 12:45:29 +0530

    Update playfair.py

--- ICS Lab/playfair.py
+++ ICS Lab/playfair.py
@@ -1,1 +1,40 @@
+k=98
+r,c=5,5
+key='word'
+l1=list()
+l2=list()
+for i in key:
+	l2.append(i)
+l2.append('a')
+l1.append(l2)
+while(k<=122):
+	l2=list()
+	for i in range(5):
+		l2.append(chr(k))
+		k=k+1
+		if(k==106 or k==100 or k==111 or k==114 or k==119):k=k+1
+	l1.append(l2)
+print(l1)
+ip='russia attacks ukraine'
+x=ip.split()
+inp=''
+for i in x:
+	inp=inp+i
+print(inp)
+i=0
+j=1
+trans_inp=''
+while(i<len(inp)):
+	while(inp[i]!=inp[j]):
+		trans_inp=trans_inp+inp[i]
+		i=i+1
+		j=j+1	
+	else:
+		trans_inp=trans_inp+'x'
+		i=i+1
+		j=j+1
+		
+	
+
+		
 
